69f16a8c37cb188ed68c76e106f727985179097e
Kyle McBurnett <mastereuclid@gmail.com>
2019-05-09 20:10:44 -0700

    small step that does nothing. why are sockets so hard

--- include/client_socket.hpp
+++ include/client_socket.hpp
@@ -1,5 +1,8 @@
 #include <string>
 #include <exception>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
 
 class bad_hostname : public std::exception {
     const char * what()const throw() override;
@@ -10,7 +13,8 @@ class sock_failed : public std::exception {
 
 class client_socket {
     private:
-    const int sock;
+    const int sock, port;
+    sockaddr_in serv_addr;
     public:
     client_socket(std::string hostname, int port);
 };
--- src/client_socket.cpp
+++ src/client_socket.cpp
@@ -3,13 +3,13 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/types.h> 
-#include <sys/socket.h>
-#include <netinet/in.h>
+
 client_socket::client_socket(std::string hostname, int port) :
-    sock(socket(AF_INET, SOCK_STREAM, 0)) {
+    sock(socket(AF_INET, SOCK_STREAM, 0)), port(port) {
     if (sock < 0) throw sock_failed();
-    
+     serv_addr.sin_family = AF_INET;
+     serv_addr.sin_addr.s_addr = INADDR_ANY;
+     serv_addr.sin_port = htons(port);
 }
 
 const char * bad_hostname::what() const throw() {
